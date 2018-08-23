3439836285d8976c98c634d2dc94ea9365d1f8e0
belindakwan <42654320+belindakwan@users.noreply.github.com>
2018-08-23 17:59:11 -0400

    Create BeyondThisPromisedLand.php

---
+++ BeyondThisPromisedLand.php
@@ -0,0 +1,410 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display:300,400,700|Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Roboto+Condensed:300,300i,400,400i,700,700i" rel="stylesheet">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+
+
+<!--
+
+font-family: 'Roboto', sans-serif;
+font-family: 'Roboto Condensed', sans-serif;
+font-family: 'Playfair Display', serif;
+
+#F800FF
+#00003A
+
+-->
+
+
+<style>
+
+
+body {
+
+font-family: 'Roboto Condensed', sans-serif;
+font-size:/*15px;*/1.15vw;
+font-weight:300;
+min-height:700px;
+overflow:hidden
+
+}
+
+.repeat {
+top:8%;
+left:0;
+position:fixed;
+width:24%;
+height:100%;
+background-color:#FFF;
+overflow:hidden;
+font-family: 'Roboto Condensed', sans-serif;
+font-weight:400;
+font-style: italic;
+font-size:/*20px;*/1.65vw;
+color:#F800FF;
+}
+
+.repeat1 {
+padding-left:10%;
+padding-top:10%;
+padding-right:0;
+position:absolute;
+overflow:auto;
+border-style:solid;
+border-width:0px;
+width:100%
+
+}
+
+.stripes1{
+
+background-image: linear-gradient(90deg, #00003a 10%, #ffffff 10%, #ffffff 50%, #00003a 50%, #00003a 60%, #ffffff 60%, #ffffff 100%);
+background-size: 10.00px 10.00px;
+  width:45%;
+  left:27%;
+  right:23%;
+  height:10%;
+  transform: skew(155deg);
+  position:absolute;
+  
+}
+
+.stripes2{
+
+background-image: linear-gradient(90deg, #00003a 10%, #ffffff 10%, #ffffff 50%, #00003a 50%, #00003a 60%, #ffffff 60%, #ffffff 100%);
+background-size: 10.00px 10.00px;
+  width:45%;
+  left:23%;
+  right:27%;
+  height:10%;
+  bottom:0%;
+  transform: skew(155deg);
+  position:absolute;
+  
+}
+
+
+h1 {
+
+font-family: 'Playfair Display', serif;
+font-size:/*20px;*/1.75vw;
+font-weight:400;float:left;
+}
+
+h2 {
+
+font-family: 'Playfair Display', serif;
+font-size:/*18px;*/1.5vw;
+font-weight:300;
+
+}
+
+h3{
+
+font-family: 'Roboto Condensed', sans-serif;
+font-size:1.25vw;/*15px;*/;
+font-weight:300;
+font-style:italic
+
+}
+
+
+.content1sidebar a {
+
+font-family: 'Roboto Condensed', sans-serif;
+font-size:1.15vw;/*15px;*/
+font-weight:300;
+text-decoration:none;
+text-transform:lowercase;
+color:white
+
+}
+
+.content2 a {
+
+font-family: 'Roboto Condensed', sans-serif;
+font-size:1.15vw;/*15px;*/
+font-weight:300;
+text-decoration:underline;
+text-transform:lowercase;
+color:black
+
+}
+
+.section1 {
+top:0;
+left:0;
+position:fixed;
+width:22%;
+height:100%;
+overflow:hidden;
+border-style:solid;
+border-color:#F800FF;
+border-width:2px;
+}
+
+
+.content1 {
+padding-left:10%;
+padding-top:10%;
+padding-right:0;
+color:black;
+position:absolute;
+overflow:auto;
+border-style:solid;
+border-width:0px;
+width:100%
+
+}
+
+.section1sidebar {
+top:0;
+left:0;
+position:fixed;
+width:22%;
+height:100%;
+background-color:#00003A;
+overflow:hidden;
+display:none
+}
+
+
+.content1sidebar {
+padding-left:10%;
+padding-top:10%;
+padding-right:0;
+color:white;
+position:absolute;
+overflow:auto;
+border-style:solid;
+border-width:0px;
+width:100%
+
+}
+
+.frame2{
+top:0;
+position:fixed;
+left:22%;
+width:45%;
+height:100%;
+background-color:white;
+border-style:solid;
+border-color:red;
+border-width:0px;
+}
+
+.section2{
+position:absolute;
+width:105%;
+height:100%;
+background-color:white;
+border-style:solid;
+border-color:blue;
+border-width:0px;
+}
+
+.content2 {
+margin:auto;
+padding-left:10.5%;
+padding-right:15%;
+top:25%;
+bottom:25%;
+height:60%;
+color:black;
+text-align:justify;
+overflow:hidden;
+position:absolute;
+display:block;
+overflow:auto;
+border-style:solid;
+border-width:0px;
+border-color:white;
+}
+
+
+.frame3{
+top:0;
+position:fixed;
+left:67%;
+width:33%;
+height:100%;
+background-color:lightblue;
+overflow:hidden;
+min-width:300px
+}
+
+.section3{
+width:104%;
+height:104%;
+overflow:auto;
+}
+
+.section3 img{
+width: 101%;
+height: auto;
+position:relative;
+margin:0;
+margin-top:-10px
+
+}
+
+.open {
+color:#F800FF;
+font-weight:300;
+/*position:absolute;
+right:10%;
+top:7%;
+z-index:20;*/
+}
+
+.close {;
+font-weight:300
+/*position:absolute;
+right:10%;
+top:7%;
+z-index:20;*/
+}
+
+::-moz-selection { /* Code for Firefox */
+    color: ;
+    background: transparent;
+}
+
+::selection {
+    color: ; 
+    background: transparent;
+}
+
+</style>
+</head>
+<body>
+
+<div class="repeat">
+<div class="repeat1">
+exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+<br>exhibitions exhibitions
+
+</div></div>
+<div class="section1">
+<div class="content1">
+<h1>Belinda 浩恩 Kwan &nbsp;&nbsp;&nbsp;<span class="open">☰</span></h1>
+
+</div></div>
+
+<div class="section1sidebar">
+<div class="content1sidebar">
+<h1>Belinda 浩恩 Kwan &nbsp;&nbsp;&nbsp;<span class="close">☰</span></h1>
+<br></br>
+<br></br>
+<br></br>
+<p><a href="">About</a></P>
+<p><a href="">Exhibitions</a>
+<p><a href="">Writing</a>
+<p><a href="">Research Projects</a>
+<p><a href="">Design</a>
+<p><a href="">CV</a>
+<p><a href="">Contact</a>
+</div></div>
+<div class="frame2">
+<div class="section2">
+<div class="stripes1"></div>
+<div class="content2">
+<h2>Beyond this Promised Land 
+<br><h3>With works by Stephanie Wu & Danika So</h3></h2>
+Fitting in. Commuting. Family homes. My house. Mom & dad. Too far, you forgot I don’t drive?
+Going to church. Going to Kristy’s house down the street. How do you say sex in Cantonese?
+Empty streets. Shoveling snow on the big driveway. I heard your daughter was dating girls.
+Open garage doors. I heard there was no word for it. Playing on the street. Subdivisions.
+Spread out. Residential neighbourhood. Suburban dream. Racism is still that bad? Driving
+everywhere. I’m concerned about her. Let’s go for Dim Sum. Thank god I’m going on vacation,
+away from all this godforsaken snow. Didn’t see you at church today. Why do you need to
+move out if you haven’t gotten married yet? Our neighbourhood is clean and quiet. I’ll get my
+daughter to talk to yours. Heard it through the grapevine. Where’d she get accepted to
+university? Oh she’s going away? 叻女 (translation: good/independent/intelligent girl, or slang
+for lesbian). Honey, we’re middle class. Oh my god, Starbucks is the classic Markham hang-out
+spot. You’re the suburban queen. I’m at my friend’s downtown, I can’t stand being in that
+suburban apartment. I can’t. I don’t understand people who stay at home - I need to fucking
+move out. I live my own life now. 性 (translation: sex). Oh shit, that’s my vibrator on the table,
+sorry.
+
+<p><a href="https://www.w3schools.com/code/tryit.asp?filename=FTEJI1J840Y6">Exhibition Publication (Download PDF)</a>
+
+
+
+</div><div class="stripes2"></div></div></div>
+<div class="frame3">
+<div class="section3">
+<div class="content3">
+<img src="https://format-com-cld-res.cloudinary.com/image/private/s--U48LyV66--/c_limit,g_center,h_550,w_65535/a_auto,fl_keep_iptc.progressive,q_95/v1/43d0c701a29aa6af9229c5917df0e61c/15.jpg?733">
+<img src="https://format-com-cld-res.cloudinary.com/image/private/s--U48LyV66--/c_limit,g_center,h_550,w_65535/a_auto,fl_keep_iptc.progressive,q_95/v1/43d0c701a29aa6af9229c5917df0e61c/15.jpg?733">
+<img src="https://format-com-cld-res.cloudinary.com/image/private/s--U48LyV66--/c_limit,g_center,h_550,w_65535/a_auto,fl_keep_iptc.progressive,q_95/v1/43d0c701a29aa6af9229c5917df0e61c/15.jpg?733">
+<img src="https://format-com-cld-res.cloudinary.com/image/private/s--U48LyV66--/c_limit,g_center,h_550,w_65535/a_auto,fl_keep_iptc.progressive,q_95/v1/43d0c701a29aa6af9229c5917df0e61c/15.jpg?733">
+<img src="https://format-com-cld-res.cloudinary.com/image/private/s--U48LyV66--/c_limit,g_center,h_550,w_65535/a_auto,fl_keep_iptc.progressive,q_95/v1/43d0c701a29aa6af9229c5917df0e61c/15.jpg?733">
+
+</div>
+</div>
+</div>
+
+ 
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $(".close").click(function(){
+        $(".section1sidebar").fadeOut(100);
+    });
+});
+</script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $(".open").click(function(){
+        $(".section1sidebar").fadeIn(100);
+    });
+});
+</script>
+
+
+</body>
+</html>
